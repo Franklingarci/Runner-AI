6954a7d6fae8518c963b215124cb5468de33e5d8
Franklin Garci <Franklingar2007@gmail.com>
2024-08-17 20:32:54 -0400

    degub

---
+++ app/api/auth/...nextauth.js
@@ -0,0 +1,30 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: "90608531680-kqar98pusuf5fbo5e5gtafrqfmv2qdq5.apps.googleusercontent.com",
+      clientSecret: "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",
+    }),
+    GitHubProvider({
+      clientId: "Ov23liWNPTPxSHXhHHLK",
+      clientSecret: "ca3a4788480654bcc3c9e0cf0197b8725caf3970",
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',  // Custom sign-in page (if any)
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      // Customize sign-in behavior if needed
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      // Customize redirect behavior if needed
+      return baseUrl;
+    },
+  },
+});
